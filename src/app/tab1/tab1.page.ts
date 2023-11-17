b1454c66832d75bfceb40364e1cece16375c0006
marlon1925 <marlon.lalangui@epn.edu.ec>
2023-11-17 16:09:35 -0500

    componentes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActionSheetController, AlertController } from '@ionic/angular';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,48 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController, private actionSheetCtrl: ActionSheetController, private router: Router) {}
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'INFO',
+      subHeader: 'BSC',
+      message: 'Hola Como estas',
+      buttons: ['Close'],
+    });
+
+    await alert.present();
+  }
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Hojas de vida',
+      buttons: [
+        {
+          text: 'Marlon',
+          role: 'destructive',
+          handler: () => {
+            this.router.navigateByUrl('/tab')
+          }
+        },
+        {
+          text: 'Josue',
+          handler: () => {
+            // Lógica para manejar la acción de compartir
+          }
+        },
+        {
+          text: 'Wilson',
+          role: 'cancel',
+          handler: () => {
+            // Lógica para manejar la acción de cancelar
+          }
+        }
+      ]
+    });
+  
+    await actionSheet.present();
+  }
+  
 }
+
+
